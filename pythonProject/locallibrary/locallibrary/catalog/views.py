7a0f15fe7490da2eaa1cb1610130d1150725cf49
Алексей Сафтдинов <gr436_saaar@lab-203-9.tomtit-tomsk.ru>
2025-11-29 11:47:23 +0700

    comment

--- pythonProject/locallibrary/locallibrary/catalog/views.py
+++ pythonProject/locallibrary/locallibrary/catalog/views.py
@@ -5,21 +5,36 @@ from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()
-    return render(
-        request,
-        'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
-    )
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+    return render(request, 'index.html', context=context)
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='war')[:5]  # Get 5 books containing the title war
 
     def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
         context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
         context['some_data'] = 'This is just some data'
         return context
 
